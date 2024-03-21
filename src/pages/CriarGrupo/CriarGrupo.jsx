8768e7b3773f92f2618e4cc735e60a3bbb57d099
Henrique Paixao <henriquemfpaixao@gmail.com>
2024-03-20 23:28:43 -0300

    criar grupo

--- src/pages/CriarGrupo/CriarGrupo.jsx
+++ src/pages/CriarGrupo/CriarGrupo.jsx
@@ -1,109 +1,84 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Logo from '../../img/logo.svg'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Menu, NativeSelect } from '@mui/material';
+import {Box,Button,Container,CssBaseline,TextField,Typography,IconButton,Paper
+} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import GroupsIcon from '@mui/icons-material/Groups';
 
-const theme = createTheme();
+export default function ForgotPassword() {
+  
+  const handleBack = () => {
+  
+  };
 
-export default function SignIn() {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-        <Paper elevation={2} sx={{ mt: 8, p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: "0" }}>
-        <Avatar
-            alt="User Image"
-            src={Logo}
-            sx={{ width: 196.11, height: 184, marginBottom: 5  }}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <IconButton onClick={handleBack} sx={{ alignSelf: 'flex-start' }}>
+          <ArrowBackIcon />
+        </IconButton>
+        <GroupsIcon
+            sx={{ width: 145, height: 145, marginBottom: 5  }}
             variant="square"
-            
         />
-          <Typography component="h1" variant="h5" style={{color :"#07382E" , marginBottom: 4, fontWeight: 'bold' }}>
-            Cadastro
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 2 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Seu Nome"
-              name="name"
-              autoComplete="email"
-              sx={{
-                '& label.Mui-focused': {
-                  color: '#07382E',
+        <Typography component="h1" variant="h5" sx={{ mt: -1, mb: 2, fontWeight: 'bold' }}>
+          Criar Grupo
+        </Typography>
+        <Typography component="h2" variant="body1" sx={{ mt: 2, mb: 2, textAlign: 'justify' }}>
+        Preparado para torna a jornada da elaboração do seu TCC mais agradavel?  
+        Primeiro vamos começar com a criação do grupo.
+        </Typography>
+        <Typography variant='subtitle2' sx={{textAlign: 'justify', }}>
+        Não se preocupe você podera altera o nome e a imagem do grupo caso necessario.
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="IDgroup-name"
+            label="Nome do Grupo"
+            name="group-name"
+            autoComplete="group-name"
+            sx={{
+              '& label.Mui-focused': {
+                color: '#07382E',
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#07382E',
                 },
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    borderColor: '#07382E',
-                  },
-                  '&:hover fieldset': {
-                    borderColor: '#07382E',
-                  },
+                '&:hover fieldset': {
+                  borderColor: '#07382E',
                 },
-              }}
-            />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="email"
-                    label="E-Mail"
-                    id="email"
-                    autoComplete="email-password"
-                    sx={{
-                        '& label.Mui-focused': {
-                            color: '#07382E',
-                        },
-                        '& .MuiOutlinedInput-root': {
-                        '& fieldset': {
-                        borderColor: '#07382E',
-                        },
-                        '&:hover fieldset': {
-                        borderColor: '#07382E',
-                        },
-                    },
-                }}
-            />
-            <Box
-                sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                component: 'form'
-                }}
-            >
-
-
-            </Box>
-
-
-
-            <Grid container>
-              <Grid item xs={12} sx={{ textAlign: 'right' }}>
-
-              </Grid>
-            </Grid>
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="outlined"
-              sx={{ mt: 3, mb: 2, backgroundColor:"#fff", color: "#07382E",  borderColor: '#07382E', '&:hover': {borderColor: '#07382E', backgroundColor: 'transparent',}, }}
-            >
-              Cadastrar
-            </Button>
-          </Box>
-        </Paper>
-    </ThemeProvider>
+              },
+              
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="outlined"
+            sx={{ mt: 3, mb: 2, backgroundColor:"#fff", color: "#07382E", borderColor: '#07382E','&:hover': {borderColor: '#07382E',backgroundColor: 'transparent', }, }}
+          >
+            Criar Grupo
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
